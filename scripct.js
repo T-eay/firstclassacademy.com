f49a1bcf5d498d151e863d4e3fa43c04c966e240
T-eay <131548671+T-eay@users.noreply.github.com>
2023-06-11 09:05:14 +0100

    Add files via upload

---
+++ scripct.js
@@ -0,0 +1,7 @@
+let menuBtn = document.querySelector('#menu-btn');
+let avrbar = document.querySelector('.header .flex .navbar');
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('fa-times');
+    NAVRBAR.CLASSlIST.TOGGLE('ACTIVE');
+}
